ee556320e56cb0e6939bd0bfc44a4935dc4917df
Geniuss <97675547+just-geniuss@users.noreply.github.com>
2025-10-11 17:04:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,16 @@ from input_part import get_user_input
 from calc_part import calculate
 from format_part import format_result
 
+def get_user_input():
+    try:
+        num1 = float(input("Введите первое число: "))
+        operation = input("Введите операцию (+, -, *, /): ")
+        num2 = float(input("Введите второе число: "))
+        return num1, operation, num2
+    except ValueError:
+        print("Ошибка: нужно вводить числа!")
+        return None, None, None
+
 def main():
     num1, operation, num2 = get_user_input()
     if num1 is None:
