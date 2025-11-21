2fc94632babd1347540212e650d6cec8defd0e0c
Evgeny <evgeny.nov.00@gmail.com>
2025-11-21 19:37:58 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="calculator",
     version="1.0.0",
     description="Calculator",
+    long_description="A simple calculator application built in Python.",
     py_modules=["main"],
     entry_points={
         "console_scripts": [
